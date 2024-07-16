1e5cfc7755a8df5a75d08bbb5870bd3bb7817458
Tonetor777 <101365568+Tonetor777@users.noreply.github.com>
2024-07-16 18:37:13 -0300

    Better solution with explanation

--- Automate-the-boring-stuff/Adding-bullets-to-wiki-markup/bulletPointAdder.py
+++ Automate-the-boring-stuff/Adding-bullets-to-wiki-markup/bulletPointAdder.py
@@ -18,4 +18,24 @@ print(newtext)
 pyperclip.copy(newtext)
 # copy the new list to clipboard 
 
+######################################################
 
+# Better version / Solution in the Book 
+
+text = pyperclip.paste()
+# Separate lines and add stars.
+lines = text.split('\n')
+for i in range(len(lines)): # loop through all indexes for "lines" list
+    lines[i] = '* ' + lines[i] # add star to each string in "lines" list
+text = '\n'.join(lines)
+pyperclip.copy(text)
+
+
+""" 
+
+In My solution:
+for line in text:
+    newtext += "* " + line + "\n"
+this is not Effecient since a string is an immutable data type each time i am appending the star to the newtext, i am not changing the value of the newtext but creating a new variable with the appended value each time. as the list number increases it becomes ineffecient. 
+
+"""
